cd5e77775af1d32533cda1997f62a46e84f0cb7d
Meenatchi Soundari <meenatchisoundari16@gmail.com>
2022-03-07 23:03:06 +0530

    first commit

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -30,21 +30,18 @@ const Footer = () => {
                     </div>
                     <div className="flexing-apps">
                         <div className="flex-cont-foot">
-                        <div className="zomato-cont">
-                            <p>sff</p>
-                            <a href="">
-                                <img className="zomato-foot" src={zomato}></img>
-                            </a>
+                            <div className="zomato-cont">
+                                    <a href="" className=""><img className="zomato-foot" src={zomato}></img></a>
+                            </div>
+                            <div className="swiggy-cont">
+                                    <a href="" >
+                                        <img className="swiggy-foot" src={swiggy}></img>
+                                    </a>
+                            </div>
                         </div>
-                        <div className="swiggy-cont">
-                            <p>sd</p>
-                            <a href="">
-                                <img className="swiggy-foot" src={swiggy}></img>
-                            </a>
-                        </div>
-                        </div>
-                        
                     </div>
+                        
+                      
                     {/* <div className="flexing">
                     <svg className="iconss" xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
                             <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
