0f7bde58e3504e877119f2bf1341282451dd959b
Meenatchi Soundari <meenatchisoundari16@gmail.com>
2022-04-02 19:41:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -19,73 +19,72 @@ import { darkTheme, lightTheme } from "./styles/theme";
 import { useEffect } from "react";
 import $ from 'jquery';
 const App = () => {
-    // useEffect(() =>{
+    useEffect(() =>{
         
-    //         $(document).on('click','.accor >  .head',function(){
-    //             `var closestDiv = $(this).closest('.accor');`
-    //             `closestDiv.find('.body').slideToggle();`
-    //         });
+            $(document).on('click','.accor >  .head',function(){
+                `var closestDiv = $(this).closest('.accor');`
+                `closestDiv.find('.body').slideToggle();`
+            });
         
-    // },[])
-    // const { theme } = useSelector((state) => state.ui);
-    // const currentTheme = theme === "light" ? lightTheme : darkTheme;
-    // const spinner = document.getElementById('spinner');
+    },[])
+    const { theme } = useSelector((state) => state.ui);
+    const currentTheme = theme === "light" ? lightTheme : darkTheme;
+    const spinner = document.getElementById('spinner');
 
-    //     if (spinner && !spinner.hasAttribute('hidden')) {
-    //     spinner.setAttribute('hidden', 'true');
-    //     }
+        if (spinner && !spinner.hasAttribute('hidden')) {
+        spinner.setAttribute('hidden', 'true');
+        }
        
     return (
         
-        <p>dkh</p>
-        // <ThemeProvider theme={currentTheme}>
-        //     <GlobalStyles />
-        //     <Helmet>
-        //         <title>Jerusha</title>
-        //         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        //         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        //         <link
-        //             href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
-        //             rel="stylesheet"
-        //         />
-        //     </Helmet>
-        //     <>
-        //         <Layout>
-        //             <Switch>
-        //                 <Route exact path="/">
-        //                     <Home />
-        //                 </Route>
-        //                 <Route exact path="/about-us">
-        //                     <About />
-        //                 </Route>
-        //                 <Route exact path="/vision">
-        //                     <Vision />
-        //                 </Route>
-        //                 <Route exact path="/video_testimonials">
-        //                     <VideoTestimonials />
-        //                 </Route>
-        //                 <Route exact path="/nutrition_food_delivery">
-        //                     <NutritionFoodDelivery />
-        //                 </Route>
-        //                 <Route exact path="/contact">
-        //                     <Contact />
-        //                 </Route>
-        //                 <Route exact path="/meals_box">
-        //                     <MealsBox />
-        //                 </Route>
-        //                 <Route exact path="/dinner_box">
-        //                     <DinnerBox />
-        //                 </Route>
-        //                 <Route exact path="/break_box">
-        //                     <BreakBox />
-        //                 </Route>
-        //                 <Route exact path="/catering">
-        //                     <CateringService />
-        //                 </Route>
-        //             </Switch>
-        //         </Layout>
-        //     </>
-        // </ThemeProvider>
+        <ThemeProvider theme={currentTheme}>
+            <GlobalStyles />
+            <Helmet>
+                <title>Jerusha</title>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+                    rel="stylesheet"
+                />
+            </Helmet>
+            <>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route exact path="/about-us">
+                            <About />
+                        </Route>
+                        <Route exact path="/vision">
+                            <Vision />
+                        </Route>
+                        <Route exact path="/video_testimonials">
+                            <VideoTestimonials />
+                        </Route>
+                        <Route exact path="/nutrition_food_delivery">
+                            <NutritionFoodDelivery />
+                        </Route>
+                        <Route exact path="/contact">
+                            <Contact />
+                        </Route>
+                        <Route exact path="/meals_box">
+                            <MealsBox />
+                        </Route>
+                        <Route exact path="/dinner_box">
+                            <DinnerBox />
+                        </Route>
+                        <Route exact path="/break_box">
+                            <BreakBox />
+                        </Route>
+                        <Route exact path="/catering">
+                            <CateringService />
+                        </Route>
+                    </Switch>
+                </Layout>
+            </>
+        </ThemeProvider>
     );
 };
 
